a62341c6f278dbc2b61179d8c91f2e2a84319615
Harsha Kittu <138769067+Kittu2412@users.noreply.github.com>
2024-05-17 06:05:43 -0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,7 @@
 # Import python packages
 # Header of the page
 import streamlit as st
+import requests
 from snowflake.snowpark.functions import col
 
 # Write directly to the app
@@ -28,6 +29,8 @@ if ingredients_list:
 
     for fruit_chosen in ingredients_list:
             ingredients_string +=fruit_chosen+ ' '
+            fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
+            fv_df=st.dataframe(data=fruityvice_response.json(),use_container_width=True)
 
     # st.write(ingredients_string)
 
@@ -44,10 +47,7 @@ if ingredients_list:
             
         st.success('Your Smoothie is ordered, ' +name_on_order +'!' ,icon="✅")
 
-import requests
-fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
-#st.text(fruityvice_response.json())
-fv_df=st.dataframe(data=fruityvice_response.json(),use_container_width=True)
+
 # option = st.selectbox(
 #     "What is your favourite fruit?",
 #     ("Banana", "Strawberries", "Peaches"))
