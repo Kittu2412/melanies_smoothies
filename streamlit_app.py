e16d7956544a4b130c34abe1810209614442d77b
Harsha Kittu <138769067+Kittu2412@users.noreply.github.com>
2024-05-17 06:33:15 -0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -17,7 +17,9 @@ st.write('The name on your Smoothie will be:',name_on_order)
 # to get the data from the table that was in the database table 
 cnx = st.connection("snowflake")
 session = cnx.session()
-my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT_NAME'))
+my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT_NAME'),col('SEARCH_ON')
+st.dataframe(data=my_dataframe, use_container_width=Ture)
+                                                                      st.stop()
 # st.dataframe(data=my_dataframe, use_container_width=True)
 
 # for the multi select option
