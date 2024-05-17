9d51a7e002ddc0b9b0c05dbf5aa782f324835e56
Harsha Kittu <138769067+Kittu2412@users.noreply.github.com>
2024-05-17 06:41:22 -0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -6,10 +6,7 @@ from snowflake.snowpark.functions import col
 
 # Write directly to the app
 st.title(":cup_with_straw: Customize Your Smoothie! :cup_with_straw:")
-st.write(
-        """Choose the fruits you want in your custom Smoothie!
-    """
-)
+st.write("""Choose the fruits you want in your custom Smoothie!""")
 
 name_on_order = st.text_input('Name on Smoothie:')
 st.write('The name on your Smoothie will be:',name_on_order)
@@ -18,9 +15,14 @@ st.write('The name on your Smoothie will be:',name_on_order)
 cnx = st.connection("snowflake")
 session = cnx.session()
 my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT_NAME'),col('SEARCH_ON'))
-st.dataframe(data=my_dataframe, use_container_width=True)
-st.stop()
 # st.dataframe(data=my_dataframe, use_container_width=True)
+# st.stop()
+# st.dataframe(data=my_dataframe, use_container_width=True)
+
+#Convert the snowpark dataframe to pandas dataframe so we can use the LOC function 
+pd_df=my_dataframe.to_pandas()
+# st.dataframe(pd_df)
+# st.stop()
 
 # for the multi select option
 ingredients_list=st.multiselect('Choose up to 5 ingredients:',my_dataframe,max_selections=5)
@@ -31,6 +33,11 @@ if ingredients_list:
 
     for fruit_chosen in ingredients_list:
             ingredients_string +=fruit_chosen+ ' '
+
+            search_on=pd.df.loc[pd.df['FRUIT_NAME'] == fruit_chosen, 'SEARCH_ON'].iloc[0]
+            st.write('The search value for ',fruit_choosen,' is ',search_on,'.')
+
+            
             st.subheader(fruit_chosen + ' Nutrition Information')
             fruityvice_response = requests.get("https://fruityvice.com/api/fruit/"+fruit_chosen)
             fv_df=st.dataframe(data=fruityvice_response.json(),use_container_width=True)
